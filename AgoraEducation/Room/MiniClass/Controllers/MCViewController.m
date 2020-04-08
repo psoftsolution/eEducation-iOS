63e8d9ab54562115e6b2b298cb4a1882508d419b
SRS <sirusheng@agora.io>
2020-04-08 13:46:46 +0800

    fix self message bug

--- AgoraEducation/Room/MiniClass/Controllers/MCViewController.m
+++ AgoraEducation/Room/MiniClass/Controllers/MCViewController.m
@@ -682,6 +682,7 @@
         MessageInfoModel *model = [MessageInfoModel new];
         model.account = EduConfigModel.shareInstance.userName;
         model.content = content;
+        model.isSelfSend = YES;
         WEAK(self);
         [self.educationManager sendMessageWithModel:model completeSuccessBlock:^{
             [weakself.messageView addMessageModel:model];
